8e8c3e033f3d4354a2d47b29a3851c33cbd1e2a4
m. nathaniel martin <matt.martin@rutgers.edu>
2016-05-27 09:04:36 -0400

    added twitter user name

--- twitterCrawl.js
+++ twitterCrawl.js
@@ -9,11 +9,12 @@ var client = new Twitter(keys.twitterKeys);
 client.stream('statuses/filter', {track: subject, lang: 'en'},  function(stream){
   stream.on('data', function(tweet) {
     var tweetTime = new Date ();
-  console.log(('____________').yellow);
+    console.log(('____________').yellow);
 
-    console.log((" @" + tweet.user.screen_name + " ").cyan + ' ::: ' + colors.cyan(tweetTime));
+    console.log(tweet.user.name.yellow + " " +(" @" + tweet.user.screen_name + " ").cyan.bold + ' ::: ' + colors.cyan(tweetTime));
     console.log(tweet.text);
   });
+
   //BULLSHIT POSH, SORRY GITHUB!
   stream.on('error', function(error) {
     console.log(error);
