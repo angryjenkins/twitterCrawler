963f5e88dbe2f7fafec08dc3ab74c8521db8c10b
m. nathaniel martin <matt@angryjenkins.com>
2017-01-30 03:59:37 -0500

    compact

--- twitterCrawl.js
+++ twitterCrawl.js
@@ -5,7 +5,6 @@ var keys      = require('./keys');
 var subject   = process.argv[2];
 var client = new Twitter(keys.twitterKeys);
 
-
 client.stream('statuses/filter', {track: subject, lang: 'en'},  function(stream){
   stream.on('data', function(tweet) {
     var tweetTime = new Date ();
@@ -14,7 +13,6 @@ client.stream('statuses/filter', {track: subject, lang: 'en'},  function(stream)
     console.log(tweet.user.name.yellow + " " +(" @" + tweet.user.screen_name + " ").cyan.bold + ' ::: ' + colors.cyan(tweetTime));
     console.log(tweet.text);
   });
-
   //BULLSHIT POSH, SORRY GITHUB!
   stream.on('error', function(error) {
     console.log(error);
