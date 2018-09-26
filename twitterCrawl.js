1742df81787b9ae20a7f0fc034dc22cadea5e45b
matt martin <matt@angryjenkins.com>
2018-09-26 12:31:56 -0400

    add moment, update date sidplay.

--- twitterCrawl.js
+++ twitterCrawl.js
@@ -1,16 +1,17 @@
 var Twitter   = require('twitter');
 var colors    = require('colors');
 var keys      = require('./keys');
+var moment = require('moment');
 
 var subject   = process.argv[2];
 var client = new Twitter(keys.twitterKeys);
 
 client.stream('statuses/filter', {track: subject, lang: 'en'},  function(stream){
   stream.on('data', function(tweet) {
-    var tweetTime = new Date ();
+    var tweetTime = moment().format("MMM Do YYYY h:mma");
     console.log(('____________').yellow);
 
-    console.log(tweet.user.name.yellow + " " +(" @" + tweet.user.screen_name + " ").red.bold + ' ::: ' + colors.red(tweetTime));
+    console.log(tweet.user.name.yellow + " " +(" @" + tweet.user.screen_name + " ").yellow.bold + ' ::: ' + colors.yellow(tweetTime));
     console.log(tweet.text);
   });
   stream.on('error', function(error) {
