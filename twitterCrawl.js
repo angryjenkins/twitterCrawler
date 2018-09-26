29d75eaa980b0977976df90bb3273b93a05b9976
matt martin <matt@angryjenkins.com>
2018-09-26 12:42:43 -0400

    install moment

--- twitterCrawl.js
+++ twitterCrawl.js
@@ -8,10 +8,10 @@ var client = new Twitter(keys.twitterKeys);
 
 client.stream('statuses/filter', {track: subject, lang: 'en'},  function(stream){
   stream.on('data', function(tweet) {
-    var tweetTime = moment().format("MMM Do YYYY h:mma");
+    var tweetTime = new Date ();
     console.log(('____________').yellow);
 
-    console.log(tweet.user.name.yellow + " " +(" @" + tweet.user.screen_name + " ").yellow.bold + ' ::: ' + colors.yellow(tweetTime));
+    console.log(tweet.user.name.yellow + " " +(" @" + tweet.user.screen_name + " ").red.bold + ' ::: ' + colors.red(tweetTime));
     console.log(tweet.text);
   });
   stream.on('error', function(error) {
