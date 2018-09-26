3d5c1e31001224605d879971f0561c858944fd9d
matt martin <matt@angryjenkins.com>
2018-09-26 13:06:32 -0400

    update colors

--- twitterCrawl.js
+++ twitterCrawl.js
@@ -9,9 +9,13 @@ var client = new Twitter(keys.twitterKeys);
 client.stream('statuses/filter', {track: subject, lang: 'en'},  function(stream){
   stream.on('data', function(tweet) {
     var tweetTime = new Date ();
-    console.log(('____________').yellow);
+    console.log(('_______________').yellow);
 
-    console.log(tweet.user.name.yellow + " " +(" @" + tweet.user.screen_name + " ").red.bold + ' ::: ' + colors.red(tweetTime));
+    console.log(tweet.user.name.yellow 
+        + " " 
+        +(" @" + tweet.user.screen_name + " ").yellow.bold 
+        + '\n::: ' + colors.yellow(tweetTime));
+
     console.log(tweet.text);
   });
   stream.on('error', function(error) {
