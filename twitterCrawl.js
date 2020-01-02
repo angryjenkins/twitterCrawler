9a43aaa43eca93df5856f9eee53b490b86d006d4
matt martin <matt@angryjenkins.com>
2020-01-02 13:20:16 -0500

    npm update?

--- twitterCrawl.js
+++ twitterCrawl.js
@@ -1,37 +1,39 @@
-require('dotenv').config()
+require('dotenv').config();
 
-var Twitter   = require('twitter');
-var colors    = require('colors');
-var keys      = require('./keys');
+var Twitter = require('twitter');
+var colors = require('colors');
+var keys = require('./keys');
 var moment = require('moment');
 
-var subject   = process.argv[2];
+var subject = process.argv[2];
 var client = new Twitter(keys.twitterKeys);
 
-client.stream('statuses/filter', {track: subject, lang: 'en'},  function(stream){
-  stream.on('data', function(tweet) {
+client.stream('statuses/filter', { track: subject, lang: 'en' }, function(
+	stream,
+) {
+	stream.on('data', function(tweet) {
+		var twit = {
+			time: '\n::: ' + moment().format('MMM Do YYYY h:mma'),
+			name: tweet.user.name,
+			handle: ' @' + tweet.user.screen_name,
+			loc: tweet => (tweet.user.location ? ' ::: ' + tweet.user.location : ''),
+			post: tweet =>
+				tweet.truncated ? tweet.extended_tweet.full_text : tweet.text,
+		};
 
-    var twit = {
-      time: '\n::: ' + moment().format("MMM Do YYYY h:mma"),
-      name: tweet.user.name,
-      handle: " @" + tweet.user.screen_name,
-      loc: (tweet) => (tweet.user.location) ? " ::: " + tweet.user.location : "",
-      post: (tweet) => (tweet.truncated) ? tweet.extended_tweet.full_text : tweet.text
-    }
+		console.log('\n_____________'.yellow);
 
+		console.log(
+			twit.name.yellow.bold +
+				' ' +
+				twit.handle.brightGreen.bold +
+				twit.time.yellow +
+				twit.loc(tweet).yellow,
+		);
 
-    console.log(('\n_____________').yellow)
-
-    console.log(twit.name.yellow.bold
-      + " " 
-      + twit.handle.green.bold
-      + twit.time.green
-      + twit.loc(tweet).green
-    )
-
-    console.log(twit.post(tweet));
-  });
-  stream.on('error', function(error) {
-    console.log(error);
-  });
+		console.log(twit.post(tweet));
+	});
+	stream.on('error', function(error) {
+		console.log(error);
+	});
 });
