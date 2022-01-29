c06b1915da289cc810fa891786111b7e78092d28
matt <matt@angryjenkins.com>
2022-01-29 17:06:33 -0500

    use chalk built-in colors

--- twitterCrawl.js
+++ twitterCrawl.js
@@ -23,7 +23,7 @@ client.stream('statuses/filter', { track: subject, lang: 'en' }, function(
 			})}`,
 			name: tweet.user.name,
 			handle: ' @' + tweet.user.screen_name,
-			desc: tweet => (tweet.user.description ? chalk.hex('#eeceee').italic(' ::: ' + tweet.user.description) : ''),
+			desc: tweet => (tweet.user.description ? chalk.greenBright.italic(' ::: ' + tweet.user.description) : ''),
 			loc: tweet => (tweet.user.location ? '::: ' + tweet.user.location : ''),
 			post: tweet => {
 				if (typeof tweet.retweeted_status === 'object' && tweet.retweeted_status !== null) {
@@ -31,7 +31,7 @@ client.stream('statuses/filter', { track: subject, lang: 'en' }, function(
 
 					const retweet = tweet.retweeted_status.truncated ? tweet.retweeted_status.extended_tweet.full_text : tweet.retweeted_status.text
 
-					return `${chalk.hex('#ddd').italic('retweeting ' + retweeted)}: ${retweet}` 
+					return `${chalk.gray.italic('retweeting ' + retweeted)}: ${retweet}` 
 				}
 
 				if (tweet.truncated === true) {
@@ -44,8 +44,8 @@ client.stream('statuses/filter', { track: subject, lang: 'en' }, function(
 
 		console.log(chalk.bold.white('\n_____________'))
 
-		console.log(`${chalk.hex('#98fb98').bold(twit.name)} ${(chalk.hex('#fff').bold(twit.handle))} ${chalk.cyanBright(twit.desc(tweet))}`)
-		console.log(`${chalk.hex('#87ceeb').italic(twit.time)} ${chalk.hex('#87ceeb').italic(twit.loc(tweet))}`)
+		console.log(`${chalk.green.bold(twit.name)} ${(chalk.yellow.bold(twit.handle))} ${chalk.cyanBright(twit.desc(tweet))}`)
+		console.log(`${chalk.blueBright.italic(twit.time)} ${chalk.cyan.italic(twit.loc(tweet))}`)
  
 		console.log('\n' + twit.post(tweet));
 	});
